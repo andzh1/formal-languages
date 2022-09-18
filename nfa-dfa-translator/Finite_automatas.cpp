7b89cbc30202f70a95d57dae76dca9da091f6e4e
andzh1 <andrey.zhizhelev@yandex.ru>
2022-09-18 12:20:40 +0300

    Removed unnesessary main()

--- nfa-dfa-translator/Finite_automatas.cpp
+++ nfa-dfa-translator/Finite_automatas.cpp
@@ -138,23 +138,3 @@ DeterministicFiniteAutomata NondeterministicFiniteAutomata::convert_to_DFA() con
     }
     return equivalent_DFA;
 }
-
-// signed main() {
-//     freopen("tests.txt", "r", stdin);
-//     // freopen("output.txt", "w", stdout);
-//     int number_of_vertices, number_of_edges;
-//     std::cin >> number_of_vertices >> number_of_edges;
-
-//     std::vector<Edge> edges(number_of_edges);
-//     for (Edge& edge : edges) {
-//         std::cin >> edge.start >> edge.end >> edge.letter;
-//     }
-//     int number_of_terminate_vertices;
-//     std::cin >> number_of_terminate_vertices;
-//     std::vector<int> terminates (number_of_terminate_vertices);
-//     for (int& index : terminates) std::cin >> index;
-//     int index_of_starting_vertice;
-//     std::cin >> index_of_starting_vertice;
-//     NondeterministicFiniteAutomata hfa(edges, number_of_vertices, terminates, index_of_starting_vertice);
-//     auto dfa = hfa.convert_to_DFA();
-// }
